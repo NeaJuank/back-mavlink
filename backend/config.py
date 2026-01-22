59afda7c0fd308dfd36d209a58e31f79e170a278
Erika Guerrero <danielmorelo002@gmail.com>
2026-01-22 15:14:37 -0500

    cambios gotty chupa pijas

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+# Cargar variables del archivo .env
+load_dotenv()
 
 MAVLINK_DEVICE = os.getenv("MAVLINK_DEVICE", "/dev/ttyACM0")  # Puerto USB de la Pixhawk en Raspberry Pi
 MAVLINK_BAUD = int(os.getenv("MAVLINK_BAUD", "57600"))
