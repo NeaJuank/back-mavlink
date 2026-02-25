34cd3cbdf0ac0094bffbb4cb686d1c82f6bb5803
Pablo <Pabloroncancio04@gmail.com>
2026-02-25 12:24:37 -0500

    ip

--- mobile/src/config.ts
+++ mobile/src/config.ts
@@ -4,5 +4,5 @@
  */
 const RASPBERRY_IP = '192.168.137.95';
 
-export const API_URL = `http://${RASPBERRY_IP}:8000`;
-export const WS_URL = `ws://${RASPBERRY_IP}:8000/ws/telemetry`;
+export const API_URL = `https://${RASPBERRY_IP}:8000`;
+export const WS_URL = `wss://${RASPBERRY_IP}:8000/ws/telemetry`;
